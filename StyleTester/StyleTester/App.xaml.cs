58277b77895d9b2e4d5307b9f23bd7323c6c37a0
HarrisonWard13 <82547522+HarrisonWard13@users.noreply.github.com>
2026-07-24 06:24:50 -0500

    Tester App
    Initial check in for app for testing the xaml styles created by the Style Creator App.

---
+++ StyleTester/StyleTester/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StyleTester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ StyleTester/StyleTester/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StyleTester
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public ObservableCollection<ListItem> Items { get; } =
+    new ObservableCollection<ListItem>
+    {
+        new ListItem { Name = "Apple", Value = "10" },
+        new ListItem { Name = "Banana", Value = "20" },
+        new ListItem { Name = "Cherry", Value = "30" }
+    };
+    public MainWindow()
+    {
+      InitializeComponent();
+      Items =
+        new ObservableCollection<ListItem>
+        {
+            new ListItem { Name = "Apple", Value = "10" },
+            new ListItem { Name = "Banana", Value = "20" },
+            new ListItem { Name = "Cherry", Value = "30" }
+        };
+    }
+  }
+
+  public class ListItem
+  {
+    public string Name { get; set; }
+    public string Value { get; set; }
+  }
+}
